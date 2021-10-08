88d120bb2f26c3f2eac82894d32d051f300ce4d3
Freyskeyd <Freyskeyd@users.noreply.github.com>
2021-10-08 22:32:08 +0000

    deploy: 9f9707adca6ae6b2ae66dc7f540871cb62162e2e

---
+++ implementors/actix/registry/trait.ArbiterService.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["event_store"] = [{"text":"impl&lt;S:&nbsp;<a class=\"trait\" href=\"event_store/prelude/trait.Storage.html\" title=\"trait event_store::prelude::Storage\">Storage</a>&gt; ArbiterService for <a class=\"struct\" href=\"event_store/prelude/struct.SubscriptionsSupervisor.html\" title=\"struct event_store::prelude::SubscriptionsSupervisor\">SubscriptionsSupervisor</a>&lt;S&gt;","synthetic":false,"types":["event_store::subscriptions::supervisor::SubscriptionsSupervisor"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
