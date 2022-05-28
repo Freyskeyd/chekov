11d73240822f000f4f22170504d9867bfae22d5f
Freyskeyd <freyskeyd@gmail.com>
2022-05-28 13:57:14 +0200

    tests: Moving pubsub tests into the tests folder

--- crates/event_store/src/subscriptions/tests/pub_sub.rs
+++ crates/event_store/src/subscriptions/tests/pub_sub.rs
@@ -1,26 +1,57 @@
-use std::time::Duration;
-
-use crate::{
-    prelude::ExpectedVersion,
-    subscriptions::{
-        fsm::{InternalFSMState, SubscriptionFSM},
-        pub_sub::PubSub,
-        tests::support::{
-            event::{EventFactory, MyEvent},
-            subscriber::SubscriberFactory,
-            EventStoreHelper,
-        },
-        StartFrom, SubscriptionNotification, SubscriptionOptions, Subscriptions,
-    },
-    InMemoryStorage,
+use crate::subscriptions::{
+    pub_sub::{PubSub, PubSubNotification},
+    tests::support::{event::EventFactory, subscriber::SubscriberFactory, EventStoreHelper},
+    SubscriptionNotification,
 };
-use actix::clock::sleep;
-use serde_json::json;
+use actix::SystemService;
+use event_store_core::versions::ExpectedVersion;
+use event_store_storage_inmemory::InMemoryStorage;
 use test_log::test;
 use uuid::Uuid;
 
 #[test(actix::test)]
-#[ignore]
+async fn should_handle_a_notification() {
+    let notification = PubSubNotification {
+        stream: "stream_name".into(),
+        events: Vec::new(),
+    };
+
+    let result = PubSub::from_registry().send(notification).await;
+
+    assert!(matches!(result, Ok(())));
+}
+
+#[test(actix::test)]
+async fn should_only_notify_subscribers() {
+    let (tracker, subscriber_addr) = SubscriberFactory::setup();
+    let (tracker_2, _) = SubscriberFactory::setup();
+    let identity = Uuid::new_v4();
+
+    PubSub::subscribe(subscriber_addr.recipient(), identity.to_string()).await;
+
+    let es = EventStoreHelper::new(InMemoryStorage::default()).await;
+    let initial = EventFactory::create_event(0);
+
+    let _ = es
+        .append(&identity, ExpectedVersion::Version(0), &[&initial])
+        .await;
+
+    let notif_1 = tracker.lock().await.pop_front();
+    assert!(
+        matches!(notif_1, Some(SubscriptionNotification::PubSubEvents(ref stream, ref events)) if !events.is_empty() && stream.as_ref() == &identity.to_string()),
+        "expected SubscriptionNotification::PubSubEvents received: {:?}",
+        notif_1
+    );
+
+    let notif_2 = tracker_2.lock().await.pop_front();
+    assert!(
+        matches!(notif_2, None),
+        "expected no notifications received: {:?}",
+        notif_2
+    );
+}
+
+#[test(actix::test)]
 async fn ignore_events_persisted_before_subscription() {
     let es = EventStoreHelper::new(InMemoryStorage::default()).await;
     let identity = Uuid::new_v4();
@@ -32,20 +63,25 @@ async fn ignore_events_persisted_before_subscription() {
         .append(&identity, ExpectedVersion::Version(0), &[&initial])
         .await;
 
-    sleep(Duration::from_millis(100)).await;
     let (tracker, subscriber_addr) = SubscriberFactory::setup();
 
     PubSub::subscribe(subscriber_addr.recipient(), identity.to_string()).await;
 
-    let x = tracker.lock().await.pop_front();
-    println!("{:?}", x);
-    assert!(matches!(x, None));
+    let notif_1 = tracker.lock().await.pop_front();
+    assert!(
+        matches!(notif_1, None),
+        "expected no notifications received: {:?}",
+        notif_1
+    );
 
     let _ = es
         .append(&identity, ExpectedVersion::Version(1), &[&event])
         .await;
 
-    let x = tracker.lock().await.pop_front();
-    println!("{:?}", x);
-    assert!(matches!(x, None));
+    let notif_2 = tracker.lock().await.pop_front();
+    assert!(
+        matches!(notif_2, Some(SubscriptionNotification::PubSubEvents(ref stream, ref events)) if !events.is_empty() && stream.as_ref() == &identity.to_string()),
+        "expected SubscriptionNotification::PubSubEvents received: {:?}",
+        notif_2
+    );
 }
